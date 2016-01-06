e5e02964823fcd5bd20f1f31a816680c137e71b9
Theophile <hemachandra.theophile@gmail.com>
2016-01-06 16:55:23 +0100

    API TRANSLATE

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,6 +6,37 @@ function MainController($scope, $http, socket, toaster,$translate) {
     {id: 0, code: 'en'},
     {id: 1, code: 'fr'},
   ]
+
+  var dynTrad = []
+  dynTrad['fr'] = {
+    thx: "Merci pour votre participation !",
+    api : {
+      'Operation completed successfully': 'Requete ok',
+      'Invalid API key': 'Cle API invalide',
+      'Blocked API key': 'Cle API bloque',
+      'Exceeded the daily limit on the number of requests': 'Operation completed successfully',
+      'Exceeded the daily limit on the amount of translated text': 'Operation completed successfully',
+      'Exceeded the maximum text size': 'Operation completed successfully',
+      'The text cannot be translated': 'Operation completed successfully',
+      'The specified translation direction is not supported': 'Operation completed successfully'
+    },
+    notFound: 'Non trouvée'
+  }
+  dynTrad['en'] = {
+    thx: "Thanks for your contribution !",
+    api : {
+      'Operation completed successfully': 'Operation completed successfully',
+      'Invalid API key': 'Invalid API key',
+      'Blocked API key': 'Blocked API key',
+      'Exceeded the daily limit on the number of requests': 'Exceeded the daily limit on the number of requests',
+      'Exceeded the daily limit on the amount of translated text': 'Exceeded the daily limit on the amount of translated text',
+      'Exceeded the maximum text size': 'Exceeded the maximum text size',
+      'The text cannot be translated': 'The text cannot be translated',
+      'The specified translation direction is not supported': 'The specified translation direction is not supported'
+    },
+    notFound: 'Not found'
+  }
+
   $scope.user =  {
     text : '',
     langue: {},
@@ -27,7 +58,7 @@ function MainController($scope, $http, socket, toaster,$translate) {
         console.log($scope.user.langue2.label);
         $scope.lang_origine = $scope.user.langue2.code;
         $scope.result = $scope.user.langue2.label;
-        toaster.pop('success', ": D", "Thanks for your contribution !");
+        toaster.pop('success', ": D", dynTrad[$scope.user.langueWebsite.code].thx);
     });
   };
 
@@ -37,7 +68,7 @@ function MainController($scope, $http, socket, toaster,$translate) {
       input: $scope.user.text,
       ouput: null
     }).then(function(res) {
-        if(res.data[0].label != "Not Found") {
+        if(res.data[0].label != dynTrad[$scope.user.langueWebsite.code].notFound) {
           $scope.lang_origine = res.data[0].code;
         }
       $scope.result_trad = null;
@@ -54,7 +85,7 @@ function MainController($scope, $http, socket, toaster,$translate) {
       $scope.result_trad = res.data.text[0];
     }).catch(function(error) {
       $scope.result_trad = "";
-      toaster.pop('error', ":(", error.data.message);
+      toaster.pop('error', ":(", dynTrad[$scope.user.langueWebsite.code].api[error.data.message]);
 
     })
   };
