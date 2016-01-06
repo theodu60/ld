bed9978ce00d70298790f3c692395fa81a367cb9
Theophile <hemachandra.theophile@gmail.com>
2016-01-06 16:10:20 +0100

    correction head dans language

--- server/api/language/language.controller.js
+++ server/api/language/language.controller.js
@@ -129,7 +129,6 @@ console.log("prediction " + prediction)
 
 // Gets a list of Languages
 exports.index = function(req, res) {
-<<<<<<< HEAD
 var nbFiles = [0,1,2,3,4,5,6,7]
 var matrix = []
 
@@ -175,12 +174,7 @@ var matrix = []
 */
 
 
-=======
 
-  var cv = require('opencv');
-  var mat = new cv.Matrix.Eye(4,4);
-  console.log(mat)
->>>>>>> origin/dev
   Language.find({}).sort({ label : 1})
     .then(responseWithResult(res))
 };
